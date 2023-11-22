17673528df19679b9484ba06108135dffcb99eb9
Rafael Dalago <rdalago@gmail.com>
2023-11-22 01:19:26 -0300

    feat: add Wine model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+  has_many :wineries
+
+  def full_location
+    "#{name}, #{country}"
+  end
 end
--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,3 +1,8 @@
 class Winery < ApplicationRecord
-  has_one :location
+  belongs_to :location
+  has_many :wines
+
+  def full_description
+    "#{name} (#{location.full_location})"
+  end
 end
